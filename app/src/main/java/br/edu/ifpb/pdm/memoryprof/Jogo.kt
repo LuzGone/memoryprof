5936cf36b1c9ecb323861dd9727eea49c5edd795
Luiz Gonzaga <luizg.academico@gmail.com>
2023-09-21 21:42:56 -0300

    Classe jogo Pronta

--- app/src/main/java/br/edu/ifpb/pdm/memoryprof/Jogo.kt
+++ app/src/main/java/br/edu/ifpb/pdm/memoryprof/Jogo.kt
@@ -14,9 +14,11 @@ class Jogo {
         "Damires","Damires");
     private var tentativas = TENTATIVAS;
     private var pontos = 0;
+    private var status = "NOVO JOGO";
 
     constructor(){
         this.professores.shuffle();
+        this.status = "JOGANDO";
     }
 
     fun jogar (palpite1:Int,palpite2:Int): Boolean{
@@ -24,12 +26,25 @@ class Jogo {
             return false
         }
         if(professores[palpite1] == professores[palpite2]){
-            this.pontos+=1;
+            this.pontos+=10;
+            if (this.pontos == 80){
+                this.status = "VENCEU";
+            }
             return true
         }else{
             this.tentativas-=1;
+            if(this.tentativas<=0){
+                this.status = "PERDEU";
+            }
             return false
         }
+    }
+
+    fun reiniciar(){
+        this.tentativas = TENTATIVAS;
+        this.professores.shuffle();
+        this.pontos = 0;
+        this.status = "JOGANDO";
     }
 
 }
